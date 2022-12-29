8b9d4ec41741d39898e2c44da0ad6379ffdff06b
UJJWAL KUMAR PANDEY <103684915+Ujjwlkumarpandey@users.noreply.github.com>
2022-12-29 16:31:31 +0530

    Add files via upload

---
+++ lab4/DuplicateCharacters.java
@@ -0,0 +1,34 @@
+
+package lab4;
+
+import java.util.Scanner;
+
+public class DuplicateCharacters {  
+     public static void main(String[] args) { 
+         Scanner string1=new Scanner(System.in);
+         System.out.println("Enter your name");   
+       String string2 = string1.next();
+        int count;  
+          
+         
+        char string[] = string2.toCharArray();  
+          
+        System.out.println("Duplicate characters in a given string: ");  
+          
+        for(int i = 0; i <string.length; i++) {  
+            count = 1;  
+            for(int j = i+1; j <string.length; j++) {  
+                if(string[i] == string[j] && string[i] != ' ') {  
+                    count++;  
+                      
+                    string[j] = '0';  
+                }  
+            }  
+             
+            if(count > 1 && string[i] != '0')  
+                System.out.println(string[i]);  
+        }  
+    }  
+} 
+    
+
