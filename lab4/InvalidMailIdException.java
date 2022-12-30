5c62934b9693f7fd29a4dbdee9b1d6d990393ce6
UJJWAL KUMAR PANDEY <103684915+Ujjwlkumarpandey@users.noreply.github.com>
2022-12-30 13:33:21 +0530

    Add files via upload

--- lab4/InvalidMailIdException.java
+++ lab4/InvalidMailIdException.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package assiment;
 
-/**
- *
- * @author acer
- */
-class InvalidMailIdException {
+public class InvalidMailIdException extends Exception {
 
-    InvalidMailIdException(String invalid_Email) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
+    public InvalidMailIdException(String invalid_Email) {
+        
+		super(invalid_Email);
+	}
 }
+    
+    
+
